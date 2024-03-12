ded017c6309eb7cb1f2725fabacca3f441590eac
Jasczlv <ianis.cozlov@gmail.com>
2024-03-12 17:29:52 +0100

    ADD separo esercizi

--- js/app.js
+++ js/app.js
@@ -38,15 +38,3 @@ function parolaPalindroma(parola) {
 // alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
 
 // ****************************************************************************************************************************
-
-// Pari e Dispari
-
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-
-// Sommiamo i due numeri
-
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-
-// Dichiariamo chi ha vinto.
---
+++ pariedispari/pariedispari.js
@@ -0,0 +1,16 @@
+// Pari e Dispari
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+let numeroUtente = prompt("inserisci numero da 1 a 5");
+if (prompt < 6) {
+  const randomNumber = Math.random() * 5;
+} else {
+  let numeroUtente = prompt("inserisci numero da 1 a 5");
+}
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+// Sommiamo i due numeri
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+// Dichiariamo chi ha vinto.
